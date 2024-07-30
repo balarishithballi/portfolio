904acfe6d9964e2594e18a22f004db48703744af
Balarishith balli <balarishith.balli.cys@gmail.com>
2024-07-31 02:51:53 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -4,16 +4,16 @@ $to = "balarishith.balli.cys@gmail.com";
 $subject = "website comment";
 
 $name = $_POST['name'];
-$surname = $_POST['surname'];
+$email = $_POST['email']; // Replaced 'surname' with 'email'
 $comment = $_POST['comment'];
 
-$message = "Name: $name\nSurname: $surname\nComment:\n$comment";
+$message = "Name: $name\nEmail: $email\nComment:\n$comment";
 
 $headers = "From: $name <$name@example.com>";
 
 if (mail($to, $subject, $message, $headers)) {
-    echo "Email sent successfully!";
+  echo "Email sent successfully!";
 } else {
-    echo "Error sending email!";
+  echo "Error sending email!";
 }
 ?>
