f8cb8c79778ca47f15e769f5e88cf8fd9e13ee8c
Balarishith balli <balarishith.balli.cys@gmail.com>
2024-07-31 20:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.getElementById('lets-talk').addEventListener('click', function() {
-    document.getElementById('contact-me').scrollIntoView({ behavior: 'smooth' });
-});
 document.addEventListener('DOMContentLoaded', function () {
-  var progressBars = document.querySelectorAll('.progress-bar');
-  progressBars.forEach(function (bar) {
-      var value = bar.getAttribute('data-value');
-      bar.style.width = value + '%';
-  });
-});
+    // Add event listener after DOM is fully loaded
+    var letsTalkButton = document.getElementById('lets-talk');
+    var contactMeSection = document.getElementById('contact-me');
 
+    if (letsTalkButton && contactMeSection) {
+        letsTalkButton.addEventListener('click', function() {
+            contactMeSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+
+    // Set progress bars' widths after DOM is fully loaded
+    var progressBars = document.querySelectorAll('.progress-bar');
+    progressBars.forEach(function (bar) {
+        var value = bar.getAttribute('data-value');
+        bar.style.width = value + '%';
+    });
+});
