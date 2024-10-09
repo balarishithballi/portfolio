152b7f5b1844734f8edab8776912194b124cf704
Balarishith balli <balarishith.balli.cys@gmail.com>
2024-10-09 12:09:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
             contactMeSection.scrollIntoView({ behavior: 'smooth' });
         });
     }
+    function openDocument() {
+    // Replace 'path/to/your/cv.pdf' with the actual path to your CV file
+    const cvUrl = 'blog/infiltr8.pdf';
+    window.open(cvUrl, '_blank'); // Open the PDF in a new tab/window
+}
+
 
     // Set progress bars' widths after DOM is fully loaded
     var progressBars = document.querySelectorAll('.progress-bar');
